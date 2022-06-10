784bd2574f208f971b955416a8b27693aed4d17d
Nikita Kozlov <sanichtw@gmail.com>
2022-06-10 03:20:58 +0300

    Added middlewares for training

--- src/store.js
+++ src/store.js
@@ -1,7 +1,23 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 
 import reducer from './reducers/reducer'
 
-const store = createStore(reducer);
+const logMiddleware = (store) => (dispatch) => (action) => {
+    console.log(action.type, store.getState())
+    return dispatch(action)
+};
+
+const stringMiddleware = () => (dispatch) => (action) => {
+    if (typeof action === 'string') {
+        return dispatch({
+            type: action
+        })
+    }
+    return dispatch(action)
+}
+
+const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
+
+store.dispatch('HELLO_WORLD')
 
 export default store;
